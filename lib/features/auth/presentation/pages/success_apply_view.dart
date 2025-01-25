de71b3ef7467dbc1b5d91def15eaae60bf0d801a
shimaa <shimaa.shams@gmail.com>
2025-01-25 20:50:32 +0200

    feat: modified UI for successfully apply

--- lib/features/auth/presentation/pages/success_apply_view.dart
+++ lib/features/auth/presentation/pages/success_apply_view.dart
@@ -1,3 +1,4 @@
+import 'package:flowery_rider/core/functions/extenstions.dart';
 import 'package:flowery_rider/core/resources/color_manager.dart';
 import 'package:flowery_rider/core/resources/routes_manager.dart';
 import 'package:flutter/material.dart';
@@ -9,36 +10,36 @@ class SuccessApplyView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.check_circle_outline,
-                color: Colors.pink,
-                size: 80,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 158,),
+            Image.asset("assets/images/check-circle.png"),
+
+            SizedBox(height: 20),
+            Text(
+              'Your application has been \n submitted!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight:FontWeight.w600,
               ),
-              SizedBox(height: 20),
-              Text(
-                'Your application has been \n submitted!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Thank you for providing your application,\n we will review your application and will\n get back to you soon.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight:FontWeight.w400,
+                color: ColorManager.grey,
               ),
-              SizedBox(height: 10),
-              Text(
-                'Thank you for providing your application,\n we will review your application and will\n get back to you soon.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                ),
-              ),
-              SizedBox(height: 30),
-              ElevatedButton(
+            ),
+            SizedBox(height: 30),
+            SizedBox(
+              width: context.screenWidth * 0.91,
+              height: context.screenHeight * 0.063,
+              child: ElevatedButton(
                 onPressed: () {
 
                   Navigator.pushNamed(context, RoutesManager.loginView);
@@ -57,8 +58,12 @@ class SuccessApplyView extends StatelessWidget {
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            Image.asset("assets/images/bgForSuccessfullyRegister.png",
+            fit: BoxFit.cover,
+            width: double.infinity,
+            ),
+          ],
         ),
       ),
     );
