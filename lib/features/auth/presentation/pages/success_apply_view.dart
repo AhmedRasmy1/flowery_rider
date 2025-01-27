b57f60cbcaa0c0dff93b31ff0e81df9e6a1ffe2a
shimaa <shimaa.shams@gmail.com>
2025-01-27 22:10:59 +0200

    feat: modified apply with country selection

--- lib/features/auth/presentation/pages/success_apply_view.dart
+++ lib/features/auth/presentation/pages/success_apply_view.dart
@@ -9,60 +9,58 @@ class SuccessApplyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 158,
-            ),
-            Image.asset("assets/images/check-circle.png"),
-            SizedBox(height: 20),
-            Text(
-              'Your application has been \n submitted!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Thank you for providing your application,\n we will review your application and will\n get back to you soon.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                color: ColorManager.grey,
-              ),
-            ),
-            SizedBox(height: 30),
-            SizedBox(
-              width: context.screenWidth * 0.91,
-              height: context.screenHeight * 0.063,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, RoutesManager.loginView);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: ColorManager.pink,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(100),
+      body: Stack(
+        children: [
+          Image.asset("assets/images/SuccessApplyBg.png", fit: BoxFit.cover, width: double.infinity,height: double.infinity,),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                Image.asset("assets/images/check-circle.png"),
+                SizedBox(height: 20),
+                Text(
+                  'Your application has been \n submitted!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
-                child: Text(
-                  'Login',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
+                SizedBox(height: 10),
+                Text(
+                  'Thank you for providing your application,\n we will review your application and will\n get back to you soon.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: ColorManager.grey,
+                  ),
                 ),
-              ),
+                SizedBox(height: 30),
+                SizedBox(
+                  width: context.screenWidth * 0.91,
+                  height: context.screenHeight * 0.063,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, RoutesManager.loginView);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: ColorManager.pink,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
+                    child: Text(
+
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Image.asset(
-              "assets/images/bgForSuccessfullyRegister.png",
-              fit: BoxFit.cover,
-              width: double.infinity,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
